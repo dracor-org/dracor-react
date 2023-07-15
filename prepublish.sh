0e201f6274b56d1d2b6d57e83a1ec47ae52af642
Carsten Milling <cmil@hashtable.de>
2023-07-15 14:15:58 +0200

    Adding a prepublish script

---
+++ prepublish.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ $(git status --porcelain --untracked-files=no) != '' ]]; then
+  echo
+  echo 'There are uncommitted changes!'
+  echo 'Please make sure to commit or revert any changes before publishing.'
+  echo
+  exit 1
+fi
+
+yarn test
+rm -r ./dist
+yarn build
