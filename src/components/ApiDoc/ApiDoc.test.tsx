c5be29014f77721014d4feaefe5ca107d77e07c4
Carsten Milling <cmil@hashtable.de>
2023-09-02 18:31:41 +0200

    Skip ApiDoc test for now
    The warnings it throws are annoyoing and  we currently cannot use
    the component  in production anyway because of some dependency conflict.

--- src/components/ApiDoc/ApiDoc.test.tsx
+++ src/components/ApiDoc/ApiDoc.test.tsx
@@ -9,7 +9,7 @@ describe('ApiDoc', () => {
   // This test currently throws a "Warning: An update to
   // Connect(WithSystem(WithErrorBoundary(Z))) inside a test was not wrapped in
   // act(...)". Run yarn test with --silent to hide those.
-  test('renders the ApiDoc component', () => {
+  test.skip('renders the ApiDoc component', () => {
     act(() => {
       render(withProviders(<ApiDoc url="ecocor.yml" title="EcoCor API" />));
     });
